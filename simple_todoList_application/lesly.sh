fc462b0194c5bcf5f14ea7b7c20a61f4e64dfd37
Cchancee <144018517+Cchancee@users.noreply.github.com>
2024-04-10 10:08:41 +0200

    f

--- simple_todoList_application/lesly.sh
+++ simple_todoList_application/lesly.sh
@@ -3,11 +3,9 @@
 # Docker login
 docker login
 
-# Build Docker image
 sudo docker build -t vite-react-app:latest .
 
 # Run Docker container 
-# docker run -p 8080:8080 vite-react-app:latest
 
 # Tag Docker image
 docker tag vite-react-app:latest cchancee/todolist_web_application:latest
@@ -17,6 +15,4 @@ sudo docker push cchancee/todolist_web_application:latest
 
 # Pull Docker image from Docker Hub
 docker pull cchancee/todolist_web_application:latest
-
-# Run Docker container with pulled image
 docker run -p 8080:8080 cchancee/todolist_web_application:latest
